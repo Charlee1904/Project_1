9ccd636b0ad36e9a9e8d11ebd8d854bf3ffbec2f
Aaron <aaronthesnowboarder@live.com>
2021-03-13 11:24:45 -0800

    api test

--- searchAPI.js
+++ searchAPI.js
@@ -1,8 +1,8 @@
 const key ='AMSm7BUhVVrEEznPAhGCTYFMmVY3KUheoNeWS4H5'
-const imageURL='https://api.nasa.gov/planetary/apod?api_key='+ key
-fetch(key)
+const imageURL='https://api.nasa.gov/planetary/apod?api_key='+key
+fetch(imageURL)
     .then(function(responce){
-        responce.json
+        responce.json()
         .then(function(data){
             console.log(data)
         })
