030367338a38fedde3bbdde4ea46e74804aab8e2
Aaron <aaronthesnowboarder@live.com>
2021-03-15 20:48:36 -0700

    added video funtionality

--- searchAPI.js
+++ searchAPI.js
@@ -1,15 +1,21 @@
 const key ='AMSm7BUhVVrEEznPAhGCTYFMmVY3KUheoNeWS4H5'
 const test='https://mooncalc.org/#/lat,lon,zoom/date/time/objectlevel/maptype'
-const imageURL='https://api.nasa.gov/planetary/apod?start_date=2012-02-11&api_key='+ key
+const imageURL='https://api.nasa.gov/planetary/apod?&api_key='+ key
+const playVidButton= document.querySelector("#potdVid");
+const myVideo=document.getElementById("potdVid")
+// const img2='https://api.nasa.gov/planetary/apod?start_date=2021-03-01&end_date=2021-03-15&api_key='+ key
 
-fetch(test)
+fetch(imageURL)
     .then(function(responce){
         responce.json()
         .then(function(data){
             console.log(data)
-            
-            // document.getElementById("potdImg").src=data[2].url
+            if(data.media_type=== "video"){
+               myVideo.src= data.url
+            }
+
+            document.getElementById("potdImg").src=data[0].url
         })
     })
 
-    
+ 
