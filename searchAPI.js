35acc9b5abfc4551d853791565988f33eedfae26
Aaron <aaronthesnowboarder@live.com>
2021-03-15 18:31:51 -0700

    comminting saved work

--- searchAPI.js
+++ searchAPI.js
@@ -1,11 +1,14 @@
 const key ='AMSm7BUhVVrEEznPAhGCTYFMmVY3KUheoNeWS4H5'
-const imageURL='https://api.nasa.gov/planetary/apod?api_key='+ key
-fetch(imageURL)
+const test='https://mooncalc.org/#/lat,lon,zoom/date/time/objectlevel/maptype'
+const imageURL='https://api.nasa.gov/planetary/apod?start_date=2012-02-11&api_key='+ key
+
+fetch(test)
     .then(function(responce){
         responce.json()
         .then(function(data){
             console.log(data)
-            document.getElementById("potdImg").src=data.url
+            
+            // document.getElementById("potdImg").src=data[2].url
         })
     })
 
