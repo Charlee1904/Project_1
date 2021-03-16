3f42d874448fd4f202238382f67368e9dbc85c62
ru14 <74753345+ru14@users.noreply.github.com>
2021-03-15 21:17:11 -0700

    sol1

--- searchAPI.js
+++ searchAPI.js
@@ -1,12 +1,21 @@
 const key ='AMSm7BUhVVrEEznPAhGCTYFMmVY3KUheoNeWS4H5'
-const imageURL='https://api.nasa.gov/planetary/apod?api_key='+ key
+const test='https://mooncalc.org/#/lat,lon,zoom/date/time/objectlevel/maptype'
+const imageURL='https://api.nasa.gov/planetary/apod?&api_key='+ key
+const playVidButton= document.querySelector("#potdVid");
+const myVideo=document.getElementById("potdVid")
+// const img2='https://api.nasa.gov/planetary/apod?start_date=2021-03-01&end_date=2021-03-15&api_key='+ key
+
 fetch(imageURL)
     .then(function(responce){
         responce.json()
         .then(function(data){
             console.log(data)
-            document.getElementById("potdImg").src=data.url
+            if(data.media_type=== "video"){
+               myVideo.src= data.url
+            }
+
+            document.getElementById("potdImg").src=data[0].url
         })
     })
 
-    
+ 
