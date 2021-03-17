46039ee3dc01dcf2e0d5ef8ecbed9c4142bed080
Aaron <aaronthesnowboarder@live.com>
2021-03-17 16:22:24 -0700

    created mars html and css page. Added the header and weather elements. Add logic to the temp and sunrise/set elements

--- searchAPI.js
+++ searchAPI.js
@@ -4,18 +4,51 @@ const imageURL='https://api.nasa.gov/planetary/apod?&api_key='+ key
 const playVidButton= document.querySelector("#potdVid");
 const myVideo=document.getElementById("potdVid")
 // const img2='https://api.nasa.gov/planetary/apod?start_date=2021-03-01&end_date=2021-03-15&api_key='+ key
+const marsWeather="https://api.maas2.apollorion.com"
+const roverImg='https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key='+key
+const earthImg='https://api.nasa.gov/EPIC/api/natural/images?api_key='+ key
 
+function homePage(){
 fetch(imageURL)
-    .then(function(responce){
-        responce.json()
-        .then(function(data){
-            console.log(data)
-            if(data.media_type=== "video"){
-               myVideo.src= data.url
+    .then(function(responceImage){
+        responceImage.json()
+        .then(function(dataImage){
+            console.log(dataImage)
+            if(dataImage.media_type=== "video"){
+                myVideo.style.display="block";
+               myVideo.src= dataImage.url;
+               
             }
-            else if(data.media_type==="image"){
-            document.getElementById("potdImg").src=data.url}
+            else if(dataImage.media_type==="image"){
+            document.getElementById("potdImg").src=dataImage.url}
         })
-    })
-
+    })}
+function marsInfo(){
+ fetch(marsWeather)
+     .then(function(responceWeather){
+         console.log(responceWeather)
+         responceWeather.json()
+         .then(function(dataWeather){
+            document.getElementById("maxTemp").textContent += " " + dataWeather.max_temp + "C"
+          document.getElementById("minTemp").textContent += " "+dataWeather.min_temp +"C"
+            document.getElementById("sunRise").textContent += " "+dataWeather.sunrise
+            document.getElementById("sunSet").textContent += " " +dataWeather.sunset
+           console.log(dataWeather)
+         })
+     })
  
+fetch(roverImg)
+     .then(function(responceRover){
+        responceRover.json()
+            .then(function(dataRover){
+                console.log(dataRover)
+            })})}
+
+// fetch(earthImg)
+//   .then(function(responceTerra){
+//      responceTerra.json()
+//       .then(function(dataTerra){
+//          console.log(dataTerra)
+//                    })})
+            
+   
