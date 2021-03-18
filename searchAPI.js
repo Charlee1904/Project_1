ddfc749d5db9b82c4480545563954ed460e8c546
Aaron <aaronthesnowboarder@live.com>
2021-03-17 20:38:13 -0700

    next page button, and back page button

--- searchAPI.js
+++ searchAPI.js
@@ -3,9 +3,18 @@ const test='https://mooncalc.org/#/lat,lon,zoom/date/time/objectlevel/maptype'
 const imageURL='https://api.nasa.gov/planetary/apod?&api_key='+ key
 const playVidButton= document.querySelector("#potdVid");
 const myVideo=document.getElementById("potdVid")
+const marsWeather="https://api.maas2.apollorion.com"
+const roverArray = ["Curiosity","Opportunity","Spirit"]
+let rover= roverArray[0]
+const earthImg='https://api.nasa.gov/EPIC/api/natural/images?api_key='+ key
+let page=1
+const nextPage = document.querySelector("#pageIncrease")
+nextPage.addEventListener("click", pageNext)
+const backPage =document.querySelector("#pageDecrease")
+backPage.addEventListener("click", returnPage)
 
-// const img2='https://api.nasa.gov/planetary/apod?start_date=2021-03-01&end_date=2021-03-15&api_key='+ key
 
+function homePage(){
 fetch(imageURL)
     .then(function(responce){
         responce.json()
@@ -17,33 +26,38 @@ fetch(imageURL)
             else if(data.media_type==="image"){
             document.getElementById("potdImg").src=data.url}
         })
-    })
+    })}
 
- 
-=======
-const marsWeather="https://api.maas2.apollorion.com"
-const roverArray = ["Curiosity","Opportunity","Spirit"]
-let rover= roverArray[0]
-const earthImg='https://api.nasa.gov/EPIC/api/natural/images?api_key='+ key
-// const changeBtn=document.querySelector('#changeRover')
-// changeBtn.addEventListener("click", changeRovers())
+ function pageNext(){
+     page +=1
+     console.log(page)
+     marsInfo()
+ }
+
+ function returnPage(){
+     page -=1
+     console.log(page)
+     marsInfo()
+ }
+
+
 
 function chaneRovers(){
-    console.log("test")
     let currentNumber=""
+    page =1
     let number =Math.round((Math.random()*3)+1);
-    console.log("Cu"+currentNumber)
-    console.log(number)
+    // console.log("Cu"+currentNumber)
+    // console.log(number)
     if (number>2){
         number = 0
     }
     if (currentNumber===number){
-        console.log('same')
+        // console.log('same')
         chaneRovers()
         return
     }
     else if(currentNumber!=number){
-        console.log('not same')
+        // console.log('not same')
         
         currentNumber=number
     }
@@ -67,21 +81,33 @@ function homePage(){
                 document.getElementById("potdImg").src=dataImage.url}
             })
         })}
-        function marsInfo(){
-            fetch(marsWeather)
-            .then(function(responceWeather){
-                console.log(responceWeather)
+ function marsInfo(){
+    if (rover===roverArray[1]&&rover===roverArray[1]){
+        console.log("aaa")
+        nextPage.style.display="none"
+        backPage.style.display="none"
+    }
+
+    if (rover===roverArray[0]){
+        nextPage.style.display="block"
+        backPage.style.display="block"
+    }
+
+
+      fetch(marsWeather)
+       .then(function(responceWeather){
                 responceWeather.json()
                 .then(function(dataWeather){
                     document.getElementById("maxTemp").textContent = "Max Temperature " + dataWeather.max_temp + "C"
                     document.getElementById("minTemp").textContent = "Min Temperature "+dataWeather.min_temp +"C"
                     document.getElementById("sunRise").textContent = "Sunrise "+dataWeather.sunrise
                     document.getElementById("sunSet").textContent = "Sunset " +dataWeather.sunset
-                    console.log(dataWeather)
+                    // console.log(dataWeather)
                 })
             })
             
-            const roverImg='https://api.nasa.gov/mars-photos/api/v1/rovers/'+rover+'/photos?sol=1000&page=1&api_key='+key
+            const roverImg='https://api.nasa.gov/mars-photos/api/v1/rovers/'+rover+'/photos?sol=1000&page='+page+'&api_key='+key
+            console.log(roverImg)
     fetch(roverImg)
     .then(function(responceRover){
         responceRover.json()
