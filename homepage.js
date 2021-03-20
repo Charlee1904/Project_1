4f51f9318a9bec22dbd1afd42c82dcd11c14905d
Aaron <aaronthesnowboarder@live.com>
2021-03-20 14:28:29 -0700

    cleaned up some code

--- homepage.js
+++ homepage.js
@@ -2,42 +2,39 @@ const key ='AMSm7BUhVVrEEznPAhGCTYFMmVY3KUheoNeWS4H5'
 const todaysImageURL='https://api.nasa.gov/planetary/apod?&api_key='+ key
 const marsPageBtn = document.querySelector("#goToMarsPage")
 marsPageBtn.addEventListener("click", marsPageFun)
-const goToDate =document.querySelector("#goToDate")
-goToDate.addEventListener("click", dateFunction)
 const datePicker = document.querySelector("#from-datepicker")
+// const goToDate =document.querySelector("#goToDate")
+// goToDate.addEventListener("click", dateFunction)
 // let userDate=""
 let userDate = 'date=' + "2021-02-01" + '&';
-const userDateUrl='https://api.nasa.gov/planetary/apod?&api_key=' + key +"&start_date="+datePicker.value+"&end_date="+datePicker.value
+const userDateUrl='https://api.nasa.gov/planetary/apod?&start_date='+userDate +"&end_date="+userDate +'&api_key='+key
 // const userDateUrl="https://api.nasa.gov/planetary/apod?api_key=MQH032jvhkJ3MB9MEone9bGtLI1fG79WCoChk16l&start_date=2010-11-28&end_date=2010-11-28"
 
 const pickedDate = document.getElementById("from-datepicker");
 pickedDate.addEventListener('change', (event) => {
     if(pickedDate.value != "") {
-        let date = moment(pickedDate.value).format("YYYY-MM-DD");
-        imageDate = 'date=' + date + '&';
-        console.log(date);
+        userDate = moment(pickedDate.value).format("YYYY-MM-DD");
+        // imageDate = 'date=' + date + '&';
+        console.log(userDate);
+        console.log(userDateUrl)
+        fetch(userDateUrl)
+        .then(function(responceInfo){
+            responceInfo.json()
+            .then(function(dataInfo)
+        {
+            console.log(dataInfo)
+            document.getElementById("potdImg").src=dataInfo.url
+            document.getElementById("titlePic").innerHTML = dataInfo.title;
+            document.getElementById("copyrightPic").innerHTML = dataInfo.copyright;
+             document.getElementById("explanationPic").innerHTML = dataInfo.explanation;
+        })
+        })
     }
 });
 
 
 
-function dateFunction(){
-    datePicker.value;
-    console.log(datePicker.value) 
-    fetch(userDateUrl)
-        .then(function(responceInfo){
-            responceInfo.json()
-            .then(function(data){
-                console.log(data)
-                document.getElementById("potdImg").src=data.url
-                document.getElementById("titlePic").innerHTML = data.title;
-                document.getElementById("copyrightPic").innerHTML = data.copyright;
-                 document.getElementById("explanationPic").innerHTML = data.explanation;
 
-                
-            })
-        })
-}
 
 
 
