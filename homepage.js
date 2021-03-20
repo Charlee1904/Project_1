538d312b3190a44b58e57d59275f146a40e24a96
Aaron <aaronthesnowboarder@live.com>
2021-03-20 11:25:16 -0700

    cleaned some work

--- homepage.js
+++ homepage.js
@@ -1,10 +1,37 @@
 const key ='AMSm7BUhVVrEEznPAhGCTYFMmVY3KUheoNeWS4H5'
-const imageURL='https://api.nasa.gov/planetary/apod?&api_key='+ key
+const todaysImageURL='https://api.nasa.gov/planetary/apod?&api_key='+ key
 const marsPageBtn = document.querySelector("#goToMarsPage")
 marsPageBtn.addEventListener("click", marsPageFun)
+const goToDate =document.querySelector("#goToDate")
+goToDate.addEventListener("click", dateFunction)
+const datePicker = document.querySelector("#from-datepicker")
+let userDate=""
+const userDateUrl='https://api.nasa.gov/planetary/apod?&api_key=' + key +"&start_date="+datePicker.value+"&end_date="+datePicker.value
+// const userDateUrl="https://api.nasa.gov/planetary/apod?api_key=MQH032jvhkJ3MB9MEone9bGtLI1fG79WCoChk16l&start_date=2010-11-28&end_date=2010-11-28"
+
+function dateFunction(){
+    datePicker.value;
+    console.log(datePicker.value) 
+    fetch(userDateUrl)
+        .then(function(responceInfo){
+            responceInfo.json()
+            .then(function(data){
+                console.log(data)
+                document.getElementById("potdImg").src=data.url
+                document.getElementById("titlePic").innerHTML = data.title;
+                document.getElementById("copyrightPic").innerHTML = data.copyright;
+                 document.getElementById("explanationPic").innerHTML = data.explanation;
+
+                
+            })
+        })
+}
+
+
+
 
 function homePage(){
-    fetch(imageURL)
+    fetch(todaysImageURL)
         .then(function(responce){
             responce.json()
             .then(function(data){
@@ -13,11 +40,11 @@ function homePage(){
                    myVideo.src= data.url
                 }
                 else if(data.media_type==="image"){
-                document.getElementById("potdImg").src=data.url}
+                document.getElementById("potdImg").src=data.url
                 document.getElementById("titlePic").innerHTML = data.title;
                 document.getElementById("copyrightPic").innerHTML = data.copyright;
                  document.getElementById("explanationPic").innerHTML = data.explanation;
-            })
+            }})
         })}
 
 
